b45ff1e3add950ffe63ba0f3ad674b4d62a26514
Nox-Nox <noxana31@gmail.com>
2022-03-03 08:32:55 +0100

    changed card height from 28vh to 30vh and in scroll module.css set gradient first color pink

--- src/components/cardView/CardView.js
+++ src/components/cardView/CardView.js
@@ -27,79 +27,77 @@ function CardView(props) {
   return (
     <ThemeProvider theme={theme}>
       {(props.items || []).map((item) => (
-
-          <Box
+        <Box
           key={item.id}
-            className = {classes.border}
-            display="flex"
-            position="relative"
-            height="28vh"
-            margin="2vw"
-            flexDirection="row"
-          >
-            <Box>
-              <CardMedia
-                alignItems="flex-end"
-                component="img"
-                image={item.coverImage.large}
-                width="10vw"
-                height="100%"
-              />
-              <Box
-                sx={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
-                position="absolute"
-                bottom="0"
-                width="10VW"
+          className={classes.border}
+          display="flex"
+          position="relative"
+          height="30vh"
+          margin="2vw"
+          flexDirection="row"
+        >
+          <Box>
+            <CardMedia
+              alignItems="flex-end"
+              component="img"
+              image={item.coverImage.large}
+              width="10vw"
+              height="100%"
+            />
+            <Box
+              sx={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
+              position="absolute"
+              bottom="0"
+              width="10VW"
+            >
+              <Typography
+                height="12vh"
+                color={"white"}
+                paddingTop="2px"
+                paddingLeft="4px"
+                paddingRight="2px"
+                fontSize="14px"
               >
-                <Typography
-                  height="10vh"
-                  color={"white"}
-                  paddingTop="2px"
-                  paddingLeft="4px"
-                  paddingRight="2px"
-                  variant="body1"
-                >
-                  {item.title.romaji}
-                </Typography>
-              </Box>
-            </Box>
-            <Box>
-              <Box
-                className={classes.scroll}
-                width="15vw"
-                alignItems="flex-end"
-                backgroundColor={mainTheme.palette.primary.light}
-                sx={{
-                  flexDirection: "column",
-                  height: "80%",
-                  maxHeight: "120%",
-                }}
-              >
-                <Typography
-                  color={"white"}
-                  fontSize="12px"
-                  variant="body2"
-                  dangerouslySetInnerHTML={{ __html: item.description }}
-                  padding="1vw"
-                />
-              </Box>
-
-              <Stack
-                alignItems="center"
-                className={classes.scroll}
-                direction="row"
-                spacing={1}
-                flexGrow={1}
-                width="15vw"
-                height="20%"
-                backgroundColor={mainTheme.palette.primary.light}
-                padding="5px"
-              >
-                <GenresChip items={item.genres} />
-              </Stack>
+                {item.title.romaji}
+              </Typography>
             </Box>
           </Box>
-        
+          <Box>
+            <Box
+              className={classes.scroll}
+              width="14vw"
+              alignItems="flex-end"
+              backgroundColor={mainTheme.palette.primary.light}
+              sx={{
+                flexDirection: "column",
+                height: "80%",
+                maxHeight: "120%",
+              }}
+            >
+              <Typography
+                color={"white"}
+                fontSize="12px"
+                variant="body2"
+                dangerouslySetInnerHTML={{ __html: item.description }}
+                padding="1vw"
+              />
+            </Box>
+
+            <Stack
+              alignItems="center"
+              className={classes.scroll}
+              direction="row"
+              spacing={1}
+              flexGrow={1}
+              width="14vw"
+              height="20%"
+              backgroundColor={mainTheme.palette.primary.light}
+              padding="5px"
+            >
+              <GenresChip items={item.genres} />
+            </Stack>
+          </Box>
+        </Box>
       ))}
     </ThemeProvider>
   );
