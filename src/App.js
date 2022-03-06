d8fec53a7c7d1746c8e254364a6edd3a36d39679
Nox-Nox <noxana31@gmail.com>
2022-03-06 14:09:08 +0100

    now the webPage will navigate to the current season anime page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,28 @@ import WinterPage from "./pages/seasonsPages/WinterPage";
 import SpringPage from "./pages/seasonsPages/SpringPage";
 import SummerPage from "./pages/seasonsPages/SummerPage";
 import FallPage from "./pages/seasonsPages/FallPage";
+import { getCurrentSeason } from "./components/Functions/GetCurrentSeason";
+
+var season = getCurrentSeason();
+
+function navigateTo(season){
+  switch(season){
+    case "WINTER":
+      return "/Anime/Winter"
+    case "SPRING":
+      return "/Anime/Spring"
+    case "SUMMER":
+      return "/Anime/Summer"
+    case "FALL":
+      return "/Anime/Fall"
+  }
+}
 
 function App() {
   return (
     
       <Routes>
-        <Route path="/" element={<Navigate to="/Anime" />} />
+        <Route path="/" element={<Navigate to={navigateTo(season)} />} />
         <Route path="/Anime" element={<AnimePage />} />
         <Route path="/Manga" element={<MangaPage />} />
         <Route path="/LightNovel" element={<LightNovelPage />} />
