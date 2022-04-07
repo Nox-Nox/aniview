3ee0f64cbf8ef48597942addf65a893ca9ed1ac7
Nox-Nox <noxana31@gmail.com>
2022-04-08 01:02:30 +0200

    changed background color from index.html with inline css

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -4,7 +4,7 @@ import { CssBaseline } from "@mui/material";
 import { Box } from "@mui/material";
 function Layout(props) {
   return (
-    <Box sx={{background: 'linear-gradient(to right, #7b29ab, #d238dd, #552586)'}}>
+    <Box>
       <CssBaseline />
       <MainNavigation />
       <main>{props.children}</main>
