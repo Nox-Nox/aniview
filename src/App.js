a43a5eaca3c5e1e18752b994dfeabd070245753b
Tahmid Nur <nox@Tahmid.localdomain>
2022-10-26 05:29:57 +0100

    removed the navigateTo function and used instead the useState and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -8,39 +8,54 @@ import SpringPage from "./pages/seasonsPages/SpringPage";
 import SummerPage from "./pages/seasonsPages/SummerPage";
 import FallPage from "./pages/seasonsPages/FallPage";
 import { getCurrentSeason } from "./components/Functions/GetCurrentSeason";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-var season = getCurrentSeason();
-
-function navigateTo(season){
-  switch(season){
-    case "WINTER":
-      return "/Home/Winter"
-    case "SPRING":
-      return "/Home/Spring"
-    case "SUMMER":
-      return "/Home/Summer"
-    case "FALL":
-      return "/Home/Fall"
-  }
-}
 
 function App() {
-  return (
-    
-      <Routes>
-        <Route path="/" element={<Navigate to={navigateTo(season)} />} />
-        <Route path="/Home" element={<HomePage />} />
-        <Route path="/Anime" element={<AnimePage />} />
-        <Route path="/Manga" element={<MangaPage />} />
-        <Route path="/LightNovel" element={<LightNovelPage />} />
+  var season = getCurrentSeason();
+  const [isSeason, setSeason] = useState("")
+  const [isLoaded, setLoaded] = useState(false)
 
-        <Route path="/Home/Winter" element={<WinterPage />} />
-        <Route path="/Home/Spring" element={<SpringPage />} />
-        <Route path="/Home/Summer" element={<SummerPage />} />
-        <Route path="/Home/Fall" element={<FallPage />} />
-      </Routes>
-    
+  useEffect(() => {
+    switch (season) {
+      case "WINTER":
+        setSeason("/Home/Winter")
+        setLoaded(true)
+      case "SPRING":
+        setSeason("/Home/Spring")
+        setLoaded(true)
+      case "SUMMER":
+        setSeason("/Home/Summer")
+        setLoaded(true)
+      case "FALL":
+        setSeason("/Home/Fall")
+        setLoaded(true)
+    }
+
+  })
+
+  if (isLoaded === false) {
+    return (
+      <div>
+        loading....
+      </div>
+    )
+  }
+  return (
+
+    <Routes>
+      <Route path="/" element={<Navigate to={isSeason} />} />
+      <Route path="/Home" element={<HomePage />} />
+      <Route path="/Anime" element={<AnimePage />} />
+      <Route path="/Manga" element={<MangaPage />} />
+      <Route path="/LightNovel" element={<LightNovelPage />} />
+
+      <Route path="/Home/Winter" element={<WinterPage />} />
+      <Route path="/Home/Spring" element={<SpringPage />} />
+      <Route path="/Home/Summer" element={<SummerPage />} />
+      <Route path="/Home/Fall" element={<FallPage />} />
+    </Routes>
+
   );
 }
 
--- src/components/NavigationBars/MainNavigation2/MainNavigation2.js
+++ src/components/NavigationBars/MainNavigation2/MainNavigation2.js
@@ -14,40 +14,43 @@ function MainNavigation2() {
     return (
         <div className={styles.wrapper}>
             <nav className={styles.navBar}>
-                <ul>
-                    <Link
-                        className={
-                            isActive === "/Home" ||
-                                isActive === "/Home/Winter" ||
-                                isActive === "/Home/Spring" ||
-                                isActive === "/Home/Summer" ||
-                                isActive === "/Home/Fall"
-                                ? styles.active
-                                : ""
-                        }
-                        to="/Home"
-                    >
-                        Home
-                    </Link>
+                <ul className={styles.menu_wrapper}>
+                    <div className={styles.menu}> </div>
+                    <div className={styles.content}>
+                        <Link
+                            className={
+                                isActive === "/Home" ||
+                                    isActive === "/Home/Winter" ||
+                                    isActive === "/Home/Spring" ||
+                                    isActive === "/Home/Summer" ||
+                                    isActive === "/Home/Fall"
+                                    ? styles.active
+                                    : ""
+                            }
+                            to="/Home"
+                        >
+                            Home
+                        </Link>
 
-                    <Link
-                        className={isActive === "/Anime" ? styles.active : ""}
-                        to="/Anime"
-                    >
-                        Anime
-                    </Link>
-                    <Link
-                        className={isActive === "/Manga" ? styles.active : ""}
-                        to="/Manga"
-                    >
-                        Manga
-                    </Link>
-                    <Link
-                        className={isActive === "/LightNovel" ? styles.active : ""}
-                        to="/LightNovel"
-                    >
-                        Light Novel
-                    </Link>
+                        <Link
+                            className={isActive === "/Anime" ? styles.active : ""}
+                            to="/Anime"
+                        >
+                            Anime
+                        </Link>
+                        <Link
+                            className={isActive === "/Manga" ? styles.active : ""}
+                            to="/Manga"
+                        >
+                            Manga
+                        </Link>
+                        <Link
+                            className={isActive === "/LightNovel" ? styles.active : ""}
+                            to="/LightNovel"
+                        >
+                            Light Novel
+                        </Link>
+                    </div>
                 </ul>
             </nav>
         </div>
